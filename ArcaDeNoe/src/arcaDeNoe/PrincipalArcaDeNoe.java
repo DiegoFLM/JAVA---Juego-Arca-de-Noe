452e135d3a3330d762ea67561fc45111fdad9303
DiegoF <diego.ledesma@correounivalle.edu.co>
2021-04-03 20:02:54 -0500

    Clase SelectorCartas completa

---
+++ ArcaDeNoe/src/arcaDeNoe/PrincipalArcaDeNoe.java
@@ -0,0 +1,16 @@
+package arcaDeNoe;
+
+public class PrincipalArcaDeNoe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SelectorCartas selector = new SelectorCartas();
+		Integer[] cartas = selector.NuevasCartas(16);
+		/*System.out.printf("Cartas: ");
+		for(int j = 0; j < cartas.length; j++) {
+			System.out.printf(cartas[j].toString() + " ");
+		}
+		System.out.printf("\n");*/
+	}
+
+}
