436b70f315d3db8692250fcc8eeb6e93f22851e4
DiegoF <diego.ledesma@correounivalle.edu.co>
2021-04-14 00:00:31 -0500

    ya está el diagrama conceptual.

---
+++ ArcaDeNoe/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"arcaDeNoe","l":"Carta"},{"p":"arcaDeNoe","l":"ControlArcaDeNoe"},{"p":"arcaDeNoe","l":"GUIArcaDeNoe"},{"p":"arcaDeNoe","l":"PrincipalArcaDeNoe"},{"p":"arcaDeNoe","l":"SelectorCartas"},{"p":"misComponentes","l":"Titulos"}];updateSearchResults();
